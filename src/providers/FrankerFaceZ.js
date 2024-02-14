8c87fe921388fcd6ed071e92bf7c3f560e210dce
ZonianMidian <57513632+ZonianMidian@users.noreply.github.com>
2024-02-14 16:22:26 -0500

    Better resolution on Mod badge in FrankerFaceZ

--- src/providers/FrankerFaceZ.js
+++ src/providers/FrankerFaceZ.js
@@ -8,7 +8,10 @@ export async function getChannel(channelID) {
 		const getChannel = await fetch(`${window.cors}https://api.frankerfacez.com/v1/room/id/${channelID}`);
 		const channelData = (await getChannel.json()).room;
 
-		let moderatorBadge = channelData.moderator_badge;
+		let moderatorBadge = null;
+		if (channelData.moderator_badge) {
+			moderatorBadge = getLastValue(channelData.mod_urls);
+		}
 		let vipBadge = null;
 		if (channelData.vip_badge) {
 			vipBadge = getLastValue(channelData.vip_badge);
