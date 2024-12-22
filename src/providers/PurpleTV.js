5db352f361852a81ea730ead569e225c7ae676ec
Zonian <57513632+ZonianMidian@users.noreply.github.com>
2024-12-22 16:28:12 -0500

    PurpleTV badges error

--- src/providers/PurpleTV.js
+++ src/providers/PurpleTV.js
@@ -1,5 +1,10 @@
 export async function getBadges() {
-	const getBadges = await fetch(`${window.cors}https://api.nopbreak.ru/orange/donations`);
+	const getBadges = await fetch(`${window.cors}https://api.nopbreak.ws/orange/donations`);
+
+	if (!getBadges.ok) {
+		throw new Error();
+	}
+
 	return await getBadges.json();
 }
 
